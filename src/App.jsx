120dc122d886e9416cf4605350520c5661721393
Tushar Sahu <tushar.sahu@kloudstac.com>
2025-09-16 13:47:13 +0530

    feat : error boundry setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 // Pages Imports
 import Home from "./pages/Home";
 
+// Error Boundary
+import ErrorBoundary from "./components/common/ErrorBoundary";
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <ErrorBoundary fallback={<div>App crashed. Try again later.</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            {/* Add other routes here */}
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
