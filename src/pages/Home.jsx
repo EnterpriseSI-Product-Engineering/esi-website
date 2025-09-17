2fd943aa6e92c4eaf127c446b4913254192da12b
Tushar Sahu <tushar.sahu@kloudstac.com>
2025-09-17 16:58:59 +0530

    test : dev branch setup'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-        Coming Soon
+        Development in progress
       </h1>
       <div className="w-[40rem] h-40 relative">
         {/* Gradients */}
