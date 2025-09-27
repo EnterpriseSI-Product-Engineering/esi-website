04e2dfcc71f255b6fadf00a2543c802b5c7462c7
Tushar Sahu <tushar.sahu@kloudstac.com>
2025-09-27 10:41:55 +0530

    update website

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import FeaturesGrid from "./components/FeaturesGrid";
+import EmailSubscription from "./components/EmailSubscription";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <main>
+        <Hero />
+        <FeaturesGrid />
+        <EmailSubscription />
+      </main>
+    </div>
+  );
+};
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
