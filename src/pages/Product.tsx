40785c182caf5116ffad7b153262435606f81183
Tushar Sahu <tushar.sahu@kloudstac.com>
2025-09-28 14:18:13 +0530

    feat : content update

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,18 @@
+import { useParams } from "react-router-dom";
+import AgenticAIBusinessPilotAccelerators from "./AgenticAIBusinessPilotAccelerators";
 import AgenticAIPlayground from "./AgenticAIPlayground";
 
 const Product = () => {
+  const { slug } = useParams();
+
   return (
     <div>
-      <AgenticAIPlayground />
+      {/* Now you can use the slug parameter to conditionally render components */}
+      {slug === "agentic-ai-builder-playground" ? (
+        <AgenticAIBusinessPilotAccelerators />
+      ) : (
+        <AgenticAIPlayground />
+      )}
     </div>
   );
 };
