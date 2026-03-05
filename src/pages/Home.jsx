0abc8c5479935df7a85c9838874f043a2db6584d
sahu-tushar <tushar.sahu@kloudstac.com>
2026-03-05 09:29:24 +0530

    feat : added button in home page to redirect to products page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,20 @@ import Hero from "../components/home/Hero";
 import VideShowCase from "../components/home/VideoShowCase";
 import Vision from "../components/home/Vision";
 import Features from "../components/home/Features";
+import { LiquidButton } from "@/components/ui/liquid-glass-button";
+import { Link } from "react-router";
+
 const Home = () => {
   return (
     <div className="container mx-auto">
       <Hero />
       <Vision />
       <Features />
+      <div className="flex items-center justify-center">
+        <Link to="/product">
+          <LiquidButton size={"lg"}>Explore our Products</LiquidButton>
+        </Link>
+      </div>
       {/* <WhatWeOffer /> */}
       {/* <VideShowCase /> */}
       {/* <EveryThingInOnePlatform /> */}
